a572d5c600259aecd03d8a2bfed560c76a006fcc
Aishik Mitra <mitraaishik2020@gmail.com>
2025-04-03 19:03:37 +0530

    designing the login page and a bit of sign in page

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=PQnbtnsYUho&t=217s
+
 import React from 'react'
 
 import {
@@ -6,11 +8,46 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 
 const App = () => {
   return (
-    <div className='text-3xl text-purple-500'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  /*
+  Usage of !!:
+  1. Convert to 'false' if 'null'
+  2. Convert to 'true' if 'not null'
+  
+  The first '!' (logical NOT) operator will convert the value into a boolean, but it inverts the value:
+  *  For example - If the value is truthy (e.g., a non-empty string, a number, etc.), ! converts it to false.
+  The second ! then reverts the inversion, ultimately giving you:
+  *  true if the value was truthy (i.e., if "token" exists in localStorage and is not an empty string).
+  */
+ 
+ // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
